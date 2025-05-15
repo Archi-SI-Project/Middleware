2252b2a6b750c6d7314cff4b9cf8db0b556f181e
Max Tonnadre <91528671+Max-Tonnadre@users.noreply.github.com>
2025-05-15 19:12:58 +0200

    endmiddleware

--- demo/demo/src/main/java/com/example/demo/API/ApiGatewayController.java
+++ demo/demo/src/main/java/com/example/demo/API/ApiGatewayController.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
 
 import java.util.Map;
@@ -27,6 +28,15 @@ public class ApiGatewayController {
     public Mono<ResponseEntity<String>> getFilms() {
         return webClient.get()
                 .uri(serviceLecture + "/movies")
+                .retrieve()
+                .toEntity(String.class);
+    }
+    //get Movie by id
+    @GetMapping("/movies/{id}")
+    @Operation(summary = "Récupérer un film par ID", description = "Délègue au service de lecture")
+    public Mono<ResponseEntity<String>> getFilm(@PathVariable Integer id) {
+        return webClient.get()
+                .uri(serviceLecture + "/movies/" + id)
                 .retrieve()
                 .toEntity(String.class);
     }
@@ -68,6 +78,53 @@ public class ApiGatewayController {
                 .toEntity(String.class);
     }
 
+    @GetMapping("/movies/search")
+    @Operation(summary = "Rechercher un film", description = "Délègue au service de lecture")
+    public Mono<ResponseEntity<String>> searchMovies(
+            @RequestParam(required = false) String genre,
+            @RequestParam(required = false) Integer minDuration,
+            @RequestParam(required = false) Integer maxDuration,
+            @RequestParam(required = false) String minCreationDate,
+            @RequestParam(required = false) String maxCreationDate,
+            @RequestParam(required = false) String searchTerm,
+            @RequestParam(required = false) String city,
+            @RequestParam(required = false) String sessionDate
+    ) {
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder
+                .fromHttpUrl(serviceLecture + "/movies/search");
+        if (genre != null) uriBuilder.queryParam("genre", genre);
+        if (minDuration != null) uriBuilder.queryParam("minDuration", minDuration);
+        if (maxDuration != null) uriBuilder.queryParam("maxDuration", maxDuration);
+        if (minCreationDate != null) uriBuilder.queryParam("minCreationDate", minCreationDate);
+        if (maxCreationDate != null) uriBuilder.queryParam("maxCreationDate", maxCreationDate);
+        if (searchTerm != null) uriBuilder.queryParam("searchTerm", searchTerm);
+        if (city != null) uriBuilder.queryParam("city", city);
+        if (sessionDate != null) uriBuilder.queryParam("sessionDate", sessionDate);
+
+        WebClient.RequestHeadersSpec<?> request = webClient.get()
+                .uri(uriBuilder.build().toUri());
+
+
+        return request.retrieve().toEntity(String.class);
+    }
+
+    @GetMapping("/users")
+    @Operation(summary = "Récupérer les utilisateurs", description = "Délègue au service de lecture")
+    public Mono<ResponseEntity<String>> getUsers(
+            @RequestHeader(value = "Authorization", required = false) String authorization) {
+
+        WebClient.RequestHeadersSpec<?> request = webClient.get()
+                .uri(serviceEcriture + "/users");
+
+        if (authorization != null && !authorization.isEmpty()) {
+            request = request.header("Authorization", authorization);
+        }
+
+        return request
+                .retrieve()
+                .toEntity(String.class);
+    }
+
 
     @PostMapping("/session/add")
     @Operation(summary = "Créer une session", description = "Délègue au service d’écriture")
@@ -87,6 +144,8 @@ public class ApiGatewayController {
                 .retrieve()
                 .toEntity(String.class);
     }
+
+
     @PostMapping("/movies/add")
     @Operation(summary = "Créer un film", description = "Délègue au service d’écriture")
     public Mono<ResponseEntity<String>> createFilm(
@@ -103,6 +162,22 @@ public class ApiGatewayController {
                 .retrieve()
                 .toEntity(String.class);
     }
+
+    @DeleteMapping("/movies/delete/{id}")
+    @Operation(summary = "Supprimer un film", description = "Délègue au service d’écriture")
+    public Mono<ResponseEntity<String>> deleteFilm(
+            @RequestHeader(value = "Authorization") String authorization,
+            @PathVariable String id) {
+        WebClient.RequestHeadersSpec<?> request = webClient.delete()
+                .uri(serviceEcriture + "/movies/delete/" + id);
+        if (authorization != null && !authorization.isEmpty()) {
+            request = request.header("Authorization", authorization);
+        }
+        return request
+                .retrieve()
+                .toEntity(String.class);
+    }
+
 
 
     @PutMapping("/movies/update/{id}")
--- demo/demo/src/main/java/com/example/demo/Config/WebClientConfig.java
+++ demo/demo/src/main/java/com/example/demo/Config/WebClientConfig.java
@@ -3,6 +3,8 @@ package com.example.demo.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebClientConfig {
@@ -11,4 +13,16 @@ public class WebClientConfig {
     public WebClient webClient() {
         return WebClient.builder().build();
     }
+
+    @Configuration
+    public class WebConfig implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**") // Allow all endpoints
+                    .allowedOrigins("http://localhost:5173") // Allow your front-end origin
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
+        }
+}
 }
